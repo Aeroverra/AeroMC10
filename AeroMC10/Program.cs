e8aaa3e6f9ffd42d2b543613ab335be6f7c1d2c7
Nicholas <88tontos@gmail.com>
2020-11-21 23:39:29 -0500

    1.16.61 Final Files

--- AeroMC10/Program.cs
+++ AeroMC10/Program.cs
@@ -16,23 +16,35 @@ namespace AeroMC10
         static Process process;
         static VAMemory vam;
         static IntPtr baseAddress;
+
         static void Main(string[] args)
         {
             Console.Title = "AeroMC10 by Aeroverra";
             Console.WriteLine("Welcome to AeroMC10 by Aeroverra");
 
             process = Process.GetProcessesByName("Minecraft.Windows").FirstOrDefault();
+
+            //Find modules in the event of multiple dll pointers
+            //var modules = process.Modules.Cast<ProcessModule>().ToList().OrderBy(x=>x.FileName).ToList();
+            //foreach (var module in modules)
+            //{
+            //    Console.WriteLine($"{module.FileName}");
+            //}
+
             vam = new VAMemory("Minecraft.Windows");
             Console.WriteLine($"Minecraft Found!");
             baseAddress = process.MainModule.BaseAddress;
             Console.WriteLine();
+            //ToggleThunder();
             while (true)
             {
+
                 var output = "";
                 Console.WriteLine("--- Menu ---");
                 Console.WriteLine("[1] Toggle InstaBreak");
                 Console.WriteLine("[2] Toggle Fly");
                 Console.WriteLine("[3] Set Anvil Cost To 1");
+                Console.WriteLine("[4] Toggle Brightness");
                 var input = Console.ReadLine();
                 int option = 0;
                 int.TryParse(input, out option);
@@ -53,6 +65,11 @@ namespace AeroMC10
                         SetAnvilCost();
                         output = "Anvil Cost Set To 1.";
                         break;
+                    case 4:
+                        ToggleBrightness();
+                        output = "Brightness Toggled";
+                        break;
+
                 }
                 Console.Clear();
                 Console.WriteLine(output);
@@ -65,14 +82,50 @@ namespace AeroMC10
 
 
         }
-        static bool AnvilIsOn = false;
-        public static void SetAnvilCost()
+        public static void ToggleThunder()
         {
-            if (AnvilIsOn)
+            //Scan for 128 when thundering and 0 when not
+            //63 on and 0 off also works  on == trident shoots lightning
+            //63 rain animation also exists  0 would be off. 62 for a very short second in between?
+            //128 and 0 also has a weather animation
+            //03656F68 798 48 28 E0 138
+
+
+            int staticPointerOffset = 0x03656F68;
+            int[] offsets = new int[] { 0x798, 0x48, 0x28, 0xE0, 0x138 };
+
+            while (true)
             {
-                AnvilIsOn = false;
+                // var pointer = FindPointer(baseAddress, staticPointerOffset, offsets);
+                var pointer = new IntPtr(0x1792582093A);
+                var value = vam.ReadInt64(pointer);
+                Console.WriteLine(value);
+            }
+        }
+        public static void ToggleBrightness()
+        {
+            //Search for float. If brightness is 66 than the float will be between .66 and .68
+            //036542E8 20 C10 B0 138 F0
+            int staticPointerOffset = 0x036542E8;
+            int[] offsets = new int[] { 0x20, 0xC10, 0xB0, 0x138, 0xF0 };
+
+            // while (true)
+            // {
+            var pointer = FindPointer(baseAddress, staticPointerOffset, offsets);
+            var value = vam.ReadFloat(pointer);
+            Console.WriteLine(value);
+            if (value > 1)
+            {
+                vam.WriteFloat(pointer, 1);
                 return;
             }
+            vam.WriteFloat(pointer, 15);
+
+
+            // }
+        }
+        public static void SetAnvilCost()
+        {
             int staticPointerOffset = 0x036A0278;
             int[] offsets = new int[] { 0x0, 0x18, 0x80, 0x5A8, 0x50, 0x20, 0xA8 };
 
@@ -83,33 +136,42 @@ namespace AeroMC10
                 var pointer = FindPointer(baseAddress, staticPointerOffset, offsets);
                 var value = vam.ReadInt32(pointer);
                 vam.WriteInt32(pointer, 1);
+                while (true)
+                {
+                    Console.WriteLine(pointer.AsHex());
+                    Thread.Sleep(5000);
+                }
                 Thread.Sleep(500);
             }
         }
-
-
-
-
+        static volatile bool FlyIsOn = false;
         public static void ToggleFly()
         {
-
+            if (FlyIsOn)
+            {
+                FlyIsOn = false;
+                return;
+            }
+            FlyIsOn = true;
             int staticPointerOffset = 0x03655728;
             int[] offsets = new int[] { 0xA8, 0x20, 0x48, 0x10, 0x58, 0x0, 0x8B8 };
 
-            var pointer = FindPointer(baseAddress, staticPointerOffset, offsets);
-
-            while (true)
+            var thread = new Thread(() =>
             {
-                var p8v2 = vam.ReadInt32(pointer);
-                Console.WriteLine($"p8v: {p8v2}");
-                vam.WriteInt32(pointer, 1);
-                Thread.Sleep(100);
-            }
+                while (FlyIsOn)
+                {
+                    var pointer = FindPointer(baseAddress, staticPointerOffset, offsets);
+                    var value = vam.ReadInt32(pointer);
+                    //Console.WriteLine($"value: {value}");
+                    vam.WriteInt32(pointer, 1);
+                    Thread.Sleep(100);
+                }
+            });
+            thread.Start();
+
 
 
         }
-
-
         public static void ToggleInstaBreak()
         {
             int staticPointerOffset = 0x3683F48;
